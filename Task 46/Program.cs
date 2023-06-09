6795470f7866d9418fc7888b602ab34a61e6defb
Gulchachak <1gulchachak05@mail.ru>
2023-06-09 17:58:53 +0300

    seminar7

---
+++ Task 46/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void FillMatrixWhithRandomNumbers(int[,] matrix) // метод для заполнения массива
+{
+    Random rnd = new Random(); // 
+    for (int i = 0; i < matrix.GetLength(0); i++) //GetLength(0)для двумерного массива определяет количество строк 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // GetLength(1) сколько будет столбцов
+        {
+            matrix[i, j] = rnd.Next(0, 11); // генерация рандомных чисел от 0 до 10
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix) // метод для печати массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} " + "\t"); // \t это табуляция \n это отступ вниз
+        }
+        System.Console.WriteLine(); // делаем пустую строку
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine()); // ввели количество строк
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine()); // ввели количество столбцов
+int[,] matrix = new int[m, n]; // создается массив
+
+FillMatrixWhithRandomNumbers(matrix); // вызов метода
+PrintMatrix(matrix); // печатаем массив
---
+++ Task 50/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        Console.Write(matrix[i, j] + "\t  ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите позицию в строке:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию в столбце:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a >= 0 && a < m && b >= 0 && b < n)
+    Console.WriteLine($"Значение элемента массива = {matrix[a, b]}");
+else
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+}
